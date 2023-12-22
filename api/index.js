12edee8fce0786f45752ee7f7b9d78eed479cefe
RayRemnant <rayremnant.me@gmail.com>
2023-12-22 15:29:52 +0000

    weird, but let's try

--- api/index.js
+++ api/index.js
@@ -4,10 +4,7 @@ puppeteer.use(StealthPlugin());
 
 module.exports = async (req, res) => {
     const browser = await puppeteer.launch({
-        args: chromium.args,
-        executablePath:
-            process.env.CHROME_EXECUTABLE_PATH || (await chromium.executablePath()),
-        headless: true,
+        headless: "new",
         // ...more config options
     });
 
